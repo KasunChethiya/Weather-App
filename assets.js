28b830373fa5a575c8c81760cc0a32627dca1c75
KasunChethiya <kcrathnamalala@gmail.com>
2024-04-10 12:38:24 +0530

    fix

---
+++ assets.js
@@ -0,0 +1,21 @@
+const apiKey = "5fc6bb0196d61ddc175653df4fa22314";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+    if (data.cod != '404') {
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    } else {
+        console.log(data.message);
+    }
+}
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
